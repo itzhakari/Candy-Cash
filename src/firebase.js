a60140dad3edf4ac9bba3d2df00cdc0846c45084
itzhakari <hakarimida@gmail.com>
2024-12-10 13:21:54 +0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_PROJECT_ID.firebaseapp.com',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_PROJECT_ID.appspot.com',
-  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-  appId: 'YOUR_APP_ID',
-  measurementId: 'YOUR_MEASUREMENT_ID',
+    apiKey: "AIzaSyAzH42-ROR5hja6hoDMYqqdrtkUi4DQLQY",
+    authDomain: "payment-history-dcb7d.firebaseapp.com",
+    projectId: "payment-history-dcb7d",
+    storageBucket: "payment-history-dcb7d.firebasestorage.app",
+    messagingSenderId: "930658374733",
+    appId: "1:930658374733:web:73ec494afe4cbf7578f804",
+    measurementId: "G-DV11PQ2TFP"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -17,4 +17,3 @@ const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
 
 export { auth, googleProvider, signInWithPopup };
-
